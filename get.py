7894180c38ad9575ec9bab4393930e8c118cd87d
Adam Kariv <adam@everything.me>
2013-07-13 21:20:10 +0300

    initial commit

---
+++ get.py
@@ -0,0 +1,54 @@
+#encoding: utf8
+
+codes = [('5800', u'\u05d4\u05d0\u05d5\u05e6\u05e8-\u05d6\u05db\u05d5\u05d9\u05d5\u05ea \u05e0\u05d9\u05e6\u05d5\u05dc\u05d9 \u05d4\u05e9\u05d5\u05d0\u05d4'), ('5700', u'\u05d4\u05de\u05e9\u05e8\u05d3 \u05dc\u05d1\u05d8\u05d7\u05d5\u05df \u05e4\u05e0\u05d9\u05dd'), ('1600', u'\u05d4\u05de\u05e9\u05e8\u05d3 \u05dc\u05d4\u05d2\u05e0\u05ea \u05d4\u05e1\u05d1\u05d9\u05d1\u05d4'), ('3000', u'\u05d4\u05de\u05e9\u05e8\u05d3 \u05dc\u05e7\u05dc\u05d9\u05d8\u05ea \u05e2\u05dc\u05d9\u05d4'), ('2200', u'\u05d4\u05de\u05e9\u05e8\u05d3 \u05dc\u05e9\u05d9\u05e8\u05d5\u05ea\u05d9 \u05d3\u05ea'), ('2080', u'\u05d7\u05d9\u05e0\u05d5\u05da-\u05d0\u05d2\u05e3 \u05dc\u05de\u05d5\u05e1\u05d3\u05d5\u05ea \u05ea\u05d5\u05e8\u05e0\u05d9\u05d9\u05dd'), ('5300', u'\u05dc\u05e9\u05db\u05ea \u05d4\u05e0\u05e9\u05d9\u05d0'), ('3200', u'\u05de. \u05d4\u05d7\u05d9\u05e0\u05d5\u05da-\u05d7\u05d9\u05e0\u05d5\u05da \u05d4\u05ea\u05d9\u05d9\u05e9\u05d1\u05d5\u05ea\u05d9'), ('0540', u'\u05de.\u05d4\u05d0\u05d5\u05e6\u05e8 - \u05de\u05d8\u05d4 \u05d4\u05d7\u05e9\u05d1 \u05d4\u05db\u05dc\u05dc\u05d9'), ('3700', u'\u05de.\u05d4\u05d0\u05d5\u05e6\u05e8 - \u05de\u05e0\u05d4\u05dc\u05ea \u05d4\u05d2\u05de\u05dc\u05d0\u05d5\u05ea'), ('0500', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d0\u05d5\u05e6\u05e8'), ('1200', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d0\u05e0\u05e8\u05d2\u05d9\u05d4 \u05d5\u05d4\u05de\u05d9\u05dd'), ('2900', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d1\u05d9\u05e0\u05d5\u05d9 \u05d5\u05d4\u05e9\u05d9\u05db\u05d5\u05df'), ('2400', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d1\u05e8\u05d9\u05d0\u05d5\u05ea'), ('2000', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d7\u05d9\u05e0\u05d5\u05da'), ('3300', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05d7\u05e7\u05dc\u05d0\u05d5\u05ea \u05d5\u05e4\u05d9\u05ea\u05d5\u05d7 \u05d4\u05db\u05e4\u05e8'), ('5200', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05de\u05d3\u05e2 \u05d5\u05d4\u05d8\u05db\u05e0\u05d5\u05dc\u05d5\u05d2\u05d9\u05d4'), ('0600', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05e4\u05e0\u05d9\u05dd'), ('2300', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05e8\u05d5\u05d5\u05d7\u05d4'), ('4000', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05ea\u05d7\u05d1\u05d5\u05e8\u05d4'), ('1100', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05ea\u05d9\u05d9\u05e8\u05d5\u05ea'), ('3600', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05ea\u05de\u05ea'), ('5000', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05ea\u05e7\u05e9\u05d5\u05e8\u05ea'), ('5210', u'\u05de\u05e9\u05e8\u05d3 \u05d4\u05ea\u05e8\u05d1\u05d5\u05ea \u05d5\u05d4\u05e1\u05e4\u05d5\u05e8\u05d8'), ('0400', u'\u05de\u05e9\u05e8\u05d3 \u05e8\u05d0\u05e9 \u05d4\u05de\u05de\u05e9\u05dc\u05d4'), ('0420', u'\u05e4\u05d9\u05ea\u05d5\u05d7 \u05e0\u05d2\u05d1, \u05d2\u05dc\u05d9\u05dc \u05d5\u05e9.\u05e4\u05e2\u05d5\u05dc\u05d4'), ('0490', u'\u05e8\u05d5\u05d4\u05de'), ('5500', u'\u05e8\u05e9\u05d5\u05ea \u05d4\u05de\u05d9\u05dd'), ('4050', u'\u05e8\u05e9\u05d5\u05ea \u05dc\u05d0\u05d5\u05de\u05d9\u05ea \u05dc\u05d1\u05d8\u05d9\u05d7\u05d5\u05ea \u05d3\u05e8\u05db\u05d9\u05dd'), ('5900', u'\u05e9\u05e8\u05d5\u05ea \u05d4\u05ea\u05e2\u05e1\u05d5\u05e7\u05d4'), ('1700', u'\u05ea\u05d9\u05d0\u05d5\u05dd \u05d4\u05e4\u05e2\u05d5\u05dc\u05d5\u05ea \u05d1\u05e9\u05d8\u05d7\u05d9\u05dd')]
+
+for k,v in codes:
+    print k,"-",v.encode('utf8')
+
+import csv
+import re
+import urllib2
+from pyquery import PyQuery as pq
+
+#print re.compile('"([0-9]+)">([^<]+)').findall("""<select name="COMP_CODE" size="1" class="sty29" id="COMP_CODE_DATA_0" onfocus="dropdown=1;" onblur="dropdown=0;" tabindex="1" onchange="dotrig(this,&quot;suppe_trg_lst_compcode&quot;,&quot;N&quot;)"><option value="0000">אנא בחר משרד/יחידה</option><option value="5800">האוצר-זכויות ניצולי השואה</option><option value="5700">המשרד לבטחון פנים</option><option value="1600">המשרד להגנת הסביבה</option><option value="3000">המשרד לקליטת עליה</option><option value="2200">המשרד לשירותי דת</option><option value="2080">חינוך-אגף למוסדות תורניים</option><option value="5300">לשכת הנשיא</option><option value="3200">מ. החינוך-חינוך התיישבותי</option><option value="0540">מ.האוצר - מטה החשב הכללי</option><option value="3700">מ.האוצר - מנהלת הגמלאות</option><option value="0500">משרד האוצר</option><option value="1200">משרד האנרגיה והמים</option><option value="2900">משרד הבינוי והשיכון</option><option value="2400">משרד הבריאות</option><option value="2000">משרד החינוך</option><option value="3300">משרד החקלאות ופיתוח הכפר</option><option value="5200">משרד המדע והטכנולוגיה</option><option value="0600">משרד הפנים</option><option value="2300">משרד הרווחה</option><option value="4000">משרד התחבורה</option><option value="1100">משרד התיירות</option><option value="3600">משרד התמת</option><option value="5000">משרד התקשורת</option><option value="5210">משרד התרבות והספורט</option><option value="0400">משרד ראש הממשלה</option><option value="0420">פיתוח נגב, גליל וש.פעולה</option><option value="0490">רוהמ</option><option value="5500">רשות המים</option><option value="4050">רשות לאומית לבטיחות דרכים</option><option value="5900">שרות התעסוקה</option><option value="1700">תיאום הפעולות בשטחים</option></select>""")
+
+DataURL = "http://tmichot.gov.il/ibi_apps/WFServlet?IBIF_webapp=/ibi_apps&IBIC_server=EDASERVE&IBIF_ex=suppe_notif_item_all6&CLICKED_ON=&LSMYEAR=%(year)s&COMP_CODE=%(hcode)s&COMP_CODE_DISPLAY=%%E4%%EE%%F9%%F8%%E3%%20%%EC%%E4%%E2%%F0%%FA%%20%%E4%%F1%%E1%%E9%%E1%%E4&NOTTYPE_LIST=FOC_NONE&LSCOMMIT_LIST=%(code)s&MDRILL=1&YEARTXT=%(year)s1231"
+CodesURL = "http://tmichot.gov.il/ibi_apps/WFServlet?IBIF_ex=runtrig&TRIGGER0=1&TRIGGER1=suppe_trg_lst_cc_commit&SW_CURROBJ=COMP_CODE&APPID=supp_ext_app&LSTFRMID=suppe_notif_item_all6&FRAMEPCT=0&DO_fex=suppe_notif_item_all6&SW_CLICKED=&LSMYEAR=%(year)s&COMP_CODE=%(hcode)s&NOTTYPE_LIST=FOC_NONE&NOTTYPE=FOC_NONE&LSMCOMMITXT=&LSCOMMIT_LIST=FOC_NONE&LSCOMMIT=FOC_NONE&OUTPUT=HTML&LSMYEAR_DISPLAY=%(year)s&COMP_CODE_DISPLAY=%%E4%%EE%%F9%%F8%%E3+%%EC%%E1%%E8%%E7%%E5%%EF+%%F4%%F0%%E9%%ED&NOTTYPE_DISPLAY=%%E4%%EB%%EC&LSCOMMIT_DISPLAY=%%E4%%EB%%EC&OUTPUT_DISPLAY=%%E4%%F6%%E2%%FA+%%E3%%E5%%E7&SW_INITIAL=N&RAND=0.9564581164158881"
+CodesRE = re.compile('set1\("([0-9]+)',re.M)
+
+out = csv.writer(file("tmichot.csv","w"))
+
+for year in range(2008,2014):
+    
+    for hcode, title in codes:
+
+        fmt = {'year':year,'hcode':hcode }
+
+        item_codes = urllib2.urlopen(CodesURL % fmt).read()
+        item_codes = CodesRE.findall(item_codes)
+        print item_codes
+
+        for item_code in item_codes:
+            fmt['code'] = item_code
+
+            print year,hcode,title.encode('utf8'),item_code
+            
+            frame = urllib2.urlopen(DataURL % fmt).read()
+            frame = pq(frame)
+            for row in frame("TR"):
+                row = pq(row)
+                _row = [year,hcode,title.encode('utf8'),item_code]
+                for x in row("TD.x3_0, TD.x3_1, TD.x2_0, TD.x2_1"):
+                    x=pq(x)
+                    x=x.text()
+                    try:
+                        _row.append(int(x.replace(",","")))
+                    except:
+                        _row.append(x.encode('utf8'))
+                if len(_row) > 4:
+                    out.writerow(_row)
+                    print _row
+
+
+#"http://tmichot.gov.il/ibi_apps/WFServlet?APPID=supp_ext_app&LSTFRMID=supp_year_notif_list&FRAMEPCT=0&DO_fex=suppe_year_notif_list&SW_CLICKED=SW_FORMOK&IBIF_ex=runuserp&LSMYEAR="+str(year)+"&COMP_CODE="+code+"&NOTTYPE_LIST=FOC_NONE&NOTTYPE=FOC_NONE&OUTPUT=HTML&LSMYEAR_DISPLAY="+str(year)+"&COMP_CODE_DISPLAY=%E4%E0%E5%F6%F8-%E6%EB%E5%E9%E5%FA+%F0%E9%F6%E5%EC%E9+%E4%F9%E5%E0%E4&NOTTYPE_DISPLAY=%E4%EB%EC&OUTPUT_DISPLAY=%E4%F6%E2%FA+%E3%E5%E7").read()
+
